974f7f896f84149fe41a324d485a12a35c05567b
hsukiasyan <h.sukiasyan@gethatch.com>
2022-07-11 23:49:23 +0200

    final app

--- App.js
+++ App.js
@@ -1,41 +1,79 @@
-import {useState} from 'react'
-import {StyleSheet, View, FlatList} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
-    const [courseGoals, setCourseGoals] =useState([])
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [courseGoals, setCourseGoals] = useState([]);
 
-    function addGoalHandler(enteredGoalText) {
-       setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, id: Math.random().toString()}])
-    }
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+    setModalIsVisible(false);
+  }
 
-    function deleteGoalHandler(id) {
-        setCourseGoals(currentCourseGoal=> {return currentCourseGoal.filter(goal=> goal.id !== id)})
-    }
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoal) => {
+      return currentCourseGoal.filter((goal) => goal.id !== id);
+    });
+  }
 
   return (
-    <View style={styles.appContainer}>
-        <GoalInput onAddGoal={addGoalHandler}/>
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer}>
+        <Button
+          title='Add New Goal'
+          color='#a065ec'
+          onPress={startAddGoalHandler}
+        />
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
         <View style={styles.goalsContainer}>
-          <FlatList data={courseGoals} renderItem={itemData => {
-              return <GoalItem text={itemData.item.text} id={itemData.item.id} onDeleteItem={deleteGoalHandler}/>}}
-                    keyExtractor={(item, index)=> {
-                        return item.id
-                    }}
-                    alwaysBounceVertical={false} />
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
         </View>
-    </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-        flex: 1,
-        paddingTop: 50,
-        paddingHorizontal: 16
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-    goalsContainer: {
-        flex: 4
-    },
+  goalsContainer: {
+    flex: 5,
+  },
 });
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,37 +1,32 @@
-function GoalItem (props) {
-    return (
+import { Text, View, StyleSheet, Pressable } from 'react-native';
 
-        <View style={styles.goalsItem}>
-            <Pressable
-                android_ripple={{color: '#210644'}}
-                onPress={props.onDeleteItem.bind(this, props.id)}
-                style={({pressed})=> pressed && styles.pressedItem}
-            >
-                <Text style={styles.goalsText}>
-                    {props.text}
-                </Text>
-            </Pressable>
-        </View>
-
-);
-
+function GoalItem(props) {
+  return (
+    <View style={styles.goalsItem}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalsText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
 }
-
-import {Text, View, StyleSheet, Pressable} from "react-native";
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
-    goalsItem: {
-        borderRadius: 6,
-        backgroundColor: '#5e0acc',
-        margin: 8,
-    },
-    pressedItem: {
-        opacity: 0.5,
-    },
-    goalsText: {
-        color: 'white',
-        padding: 8,
-    }
-})
+  goalsItem: {
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    margin: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalsText: {
+    color: 'white',
+    padding: 8,
+  },
+});
